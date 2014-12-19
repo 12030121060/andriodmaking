b8b09f5f00f1fa06b432fa33664ecbe67ebc3ca4
samiyaparvez <samiya.aquarius@gmail.com>
2014-12-19 11:27:13 +0530

    project

--- src/com/example/eventhandling/MainActivity.java
+++ src/com/example/eventhandling/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.eventhandling;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,10 @@ public class MainActivity extends Activity {
 
 	private Button btclick;
 	private EditText name;
+	private Button bt;
+	private Button br;
+	private EditText t1;
+
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +25,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.applayout);
         btclick=(Button)findViewById(R.id.btclick);
         name=(EditText)findViewById(R.id.edit12);
+        t1=(EditText)findViewById(R.id.edit23);
         OnClickListener l1=new OnClickListener() {
 			
-			@Override
+			@SuppressLint("DefaultLocale") @Override
 			/*public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				btclick.setText("Button was clicked");
@@ -31,8 +37,12 @@ public class MainActivity extends Activity {
 				Toast.makeText(getApplicationContext(), "ButtonClicked",
 						Toast.LENGTH_LONG).show();
 			}*/
-			public void onClick(View a1){
+			/*public void onClick(View a15){
 				Toast.makeText(getApplicationContext(), name.getText(),
+						Toast.LENGTH_LONG).show();
+			}*/
+			public void onClick(View a12){
+				Toast.makeText(getApplicationContext(),t1.toString().toLowerCase(), 
 						Toast.LENGTH_LONG).show();
 			}
 		};
